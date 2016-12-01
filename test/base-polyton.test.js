b680fb18645a2a95a20f5cd5e84a1c27161cc410
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-01 14:38:31 +0100

    Tested BasePolyton with type FullName (Duplicated tests with type Name and adapted them.)

--- test/base-polyton.test.js
+++ test/base-polyton.test.js
@@ -74,3 +74,78 @@ describe('Testing BasePolyton on type Name', function() {
   });
 
 });
+
+describe('Testing BasePolyton on type FullName', function() {
+
+  beforeEach(function() {
+    class FullName {
+      constructor(firstname, lastname) {
+        this.setName(firstname, lastname);
+      }
+
+      setName(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+      }
+      getName() {
+        return this.firstname + ' ' + this.lastname;
+      }
+    }
+
+    this.FullName = FullName;
+    this.BasePolyton = BasePolytonFactory(FullName);
+  });
+
+  it(`Unique argument (['Jamy', 'Doe'])`, function() {
+
+    const BasePolyton = this.BasePolyton;
+
+    const p = new BasePolyton(['Jamy', 'Doe']);
+    expect(p.length).to.equal(1);
+    expect(p.elements).to.be.instanceof(Array);
+    expect(p.elements[0]).not.to.be.undefined;
+    expect(p.elements[0]).to.be.instanceof(this.FullName);
+    expect(p.elements[0].getName()).to.equal('Jamy Doe');
+
+    const name = p.at(0);
+    expect(name.getName()).to.equal('Jamy Doe');
+
+    name.setName('Henry', 'Ford');
+    expect(name.getName()).not.to.equal('Jamy Doe');
+    expect(p.at(0).getName()).to.equal('Henry Ford');
+    expect(p.at(0)).to.equal(name);
+
+    expect(p.get('Henry', 'Ford')).to.be.undefined;
+    expect(p.get('Jamy', 'Doe')).to.equal(name);
+
+  });
+
+  it(`Arguments ('Jamy', 'Henry', 'Carla')`, function() {
+
+    const BasePolyton = this.BasePolyton;
+    const names = [['Jamy', 'Doe'], ['Henry', 'Ford'], ['Carla', 'Stone']];
+
+    const p = new BasePolyton(...names);
+    expect(p.length).to.equal(3);
+    expect(p.elements).to.be.instanceof(Array);
+
+    p.elements.forEach((el, i) => {
+      expect(el).not.to.be.undefined;
+      expect(el).to.be.instanceof(this.FullName);
+      expect(el.getName()).to.equal(names[i].join(' '));
+
+      const name = p.at(i);
+      expect(name.getName()).to.equal(names[i].join(' '));
+
+      name.setName('George', 'Bleep');
+      expect(name.getName()).not.to.equal(names[i].join(' '));
+      expect(p.at(i).getName()).to.equal('George Bleep');
+      expect(p.at(i)).to.equal(name);
+
+      expect(p.get('George', 'Bleep')).to.be.undefined;
+      expect(p.get(...names[i])).to.equal(name);
+    });
+
+  });
+
+});
