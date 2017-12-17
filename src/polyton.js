316fadcd7af160611d3a43261ecc04f1690338b0
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-17 02:30:48 +0100

    Fixed typo

--- src/polyton.js
+++ src/polyton.js
@@ -7,7 +7,7 @@ Singleton: S(x) = y && y=y' => x=x'
 SingletonFactory: F(Y,X) = S
 BasePolyton: B(x,x',x"...) = [S(x),S(x'),S(x")...] = [y,y',y"...] = b
 BasePolytonFactory: G(Y,X) = B
-Polyton: P(x1,x2,x2) = b && b=b' => x1=)x1', x2=x2', x3=x3'
+Polyton: P(x1,x2,x2) = b && b=b' => x1=x1', x2=x2', x3=x3'
 PolytonFactory: H(Y,X) = P
 
 A Polyton is a singleton array of like singletons
